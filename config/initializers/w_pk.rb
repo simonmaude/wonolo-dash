d77e840c562473bb1befdf691639af5cca7122cf
Simon <simonmaude@gmail.com>
2017-09-28 00:00:09 +0000

    maint: added API key storage in secrets.yml

---
+++ config/initializers/w_pk.rb
@@ -0,0 +1,3 @@
+W_pk.configure do |config|
+  config.api_key = Rails.application.secrets.w_pk_key
+end
---
+++ config/initializers/w_sk.rb
@@ -0,0 +1,3 @@
+W_sk.configure do |config|
+  config.api_key = Rails.application.secrets.w_sk_key
+end
